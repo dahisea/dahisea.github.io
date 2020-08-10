3707aff2dbecd82f5b0cc7e5f81dca8e31b9a123
devseaio <69105967+devseaio@users.noreply.github.com>
2020-08-10 09:54:56 +0800

    Update General.js

--- js/General.js
+++ js/General.js
@@ -158,3 +158,16 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 }
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https'){
+   bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  }
+  else{
+  bp.src = 'https://push.zhanzhang.baidu.com/push.js';
+  }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+}
